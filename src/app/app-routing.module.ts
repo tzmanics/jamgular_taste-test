b1809e5f11bc18422dd6cdd0fc66199d9a204f20
Tara Z. Manicsic <tzmanics@gmail.com>
2019-10-20 21:18:47 -0400

    connects nav and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BestPracticesComponent } from './best-practices/best-practices.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ExamplesComponent } from './examples/examples.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'best-practices', component: BestPracticesComponent},
+  { path: 'resources', component: ResourcesComponent},
+  { path: 'examples', component: ExamplesComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
